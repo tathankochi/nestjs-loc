fe0dd93f07978078e0224e9f5b282909ae02d436
LAPTOP-0RUBD428\XLOC <tathankochi@gmail.com>
2025-09-01 13:26:44 +0700

    feat(main): add API versioning configuration

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { ConfigService } from '@nestjs/config';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { JwtAuthGuard } from './auth/jwt-auth-guard';
 import { TransformInterceptor } from './core/transform.interceptor';
 
@@ -25,6 +25,14 @@ async function bootstrap() {
     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
     "preflightContinue": false,
   });
+
+  //config versioning
+  app.setGlobalPrefix('api');
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
+
   await app.listen(configService.get('PORT'));
 }
 bootstrap();
